31192c2956f000225bcbcee84a0f2c6521ea9f86
devanshuchaudhary <devanshu88@gmail.com>
2023-02-13 17:51:50 +0530

    Initial Commit

---
+++ NorrisFacts/NorrisFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorrisFactsApp.swift
+//  NorrisFacts
+//
+//  Created by dddiehard on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NorrisFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
